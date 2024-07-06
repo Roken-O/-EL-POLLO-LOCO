a91f9c7981d351b6a0f9f2b1e413e42dfb6f1af3
Roken-O <roken.othman91@gmail.com>
2024-07-06 21:58:12 +0200

    added character and chicken

--- js/game.js
+++ js/game.js
@@ -1,13 +1,10 @@
 let canvas;
-let ctx;
-let world = new World();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    // character.src = '../img/2_character_pepe/2_walk/W-21.png';
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
-    // console.log('My Character is', character); ما عاد يشتفل
     console.log('My World is', world['character']);
     
 }
--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,5 +1,11 @@
 class Chicken extends MovableObject{
 
+    constructor(){
+        super().loadImage('img/4_enemie_boss_chicken/1_walk/G1.png');
+
+        this.x = 200 + Math.random()*500;
+    }
+
     eat(){
 
     }
